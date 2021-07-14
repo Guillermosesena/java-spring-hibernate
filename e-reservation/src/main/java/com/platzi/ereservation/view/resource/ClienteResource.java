6c69b652ac20d58194aef6fcd415c1403d4cf7eb
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-13 20:55:54 -0500

    27/38 Spring Rest

---
+++ e-reservation/src/main/java/com/platzi/ereservation/view/resource/ClienteResource.java
@@ -0,0 +1,14 @@
+package com.platzi.ereservation.view.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Clase que representa el servicio web de cliente 
+ * 
+ * **/
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteResource {
+
+}
