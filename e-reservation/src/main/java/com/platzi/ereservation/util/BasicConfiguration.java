953af26d344642087af7ad4df780d3ad624c464d
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-14 00:48:23 -0500

    33/38 Login page

--- e-reservation/src/main/java/com/platzi/ereservation/util/BasicConfiguration.java
+++ e-reservation/src/main/java/com/platzi/ereservation/util/BasicConfiguration.java
@@ -1,9 +1,13 @@
 package com.platzi.ereservation.util;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /***
  * Clase para manejar la seguridad
@@ -15,8 +19,19 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		.anyRequest().authenticated()
 		.and()
-		.formLogin()
-		.and()
-		.httpBasic();
+		.formLogin().loginPage("/app/login").permitAll()
+		.failureUrl("/app/login?error=true")
+		.defaultSuccessUrl("/app/home", true);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("guillermo").password(encoder.encode("platzi")).roles("USER");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/static_/**", "/css/**", "/js/**", "/images/**");
 	}
 }
